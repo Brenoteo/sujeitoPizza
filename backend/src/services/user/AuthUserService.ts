a03d72b21accd953d274d3b9af3339bddc85b806
Brenoteo <breno-teodoro@outlook.com>
2023-07-27 12:07:37 -0300

    encrypted password

---
+++ backend/src/services/user/AuthUserService.ts
@@ -0,0 +1,36 @@
+
+import prismaClient from "../../prisma";
+import { compare } from "bcryptjs";
+
+interface AuthRequest {
+  email: string;
+  password: string;
+}
+
+class AuthUserService {
+  async execute({ email, password }: AuthRequest){
+    // Verifica se o email existe
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: email
+      }
+    })
+
+    if (!user){
+      throw new Error("User/password incorrect")
+    }
+
+    // Verifica se a senha está correta
+    const passwordMatch = await compare(password, user.password)
+
+    if(!passwordMatch){
+      throw new Error("User/password incorrect")
+    }
+
+    
+
+    return {ok: true}
+  }
+}
+
+export { AuthUserService };
